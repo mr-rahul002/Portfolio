b0f3157811dd1e7c2fbc12226ce948f87b5d4ca1
mr-rahul002 <02rahulseth@gmail.com>
2025-03-30 21:11:56 +0530

    publish changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: true, // Allows local network access
     port: 8080,
   },
   plugins: [react()],
-  base: "/Portfolio/", // Ensure it matches your repo name
+  base: "/Portfolio", // Ensure this matches your repo name
   resolve: {
     alias: {
-      "@": "/src", // Vite supports direct paths without 'path' module
+      "@": "/src", // Vite allows direct aliasing without using `path`
     },
   },
-}));
+});
